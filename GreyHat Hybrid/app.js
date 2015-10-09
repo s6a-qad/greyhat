94252cd2ee04c3aba1e887a53756ffd324020a59
Smita Agarwal <s6a@qad.com>
2015-10-09 23:47:31 +0000

    Changes made by Smita Agarwal (s6a@qad.com).

--- GreyHat Hybrid/app.js
+++ GreyHat Hybrid/app.js
@@ -1,6 +1,7 @@
 (function() {
     // store a reference to the application object that will be created
     // later on so that we can use it if need be
+    // comment added by Smita
     var app = {
         data: {}
     };
